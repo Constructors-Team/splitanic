f57d0743873e914084e2fa533d84bb328e36bf47
ansermgw <25158748+ansermgw@users.noreply.github.com>
2025-03-14 21:34:56 +0100

    feat: add basic boat

---
+++ Assets/Code/Scripts/BoatController.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BoatController : MonoBehaviour
+{
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float rotationSpeed;
+    
+    private KeyCode keyForward;
+    private KeyCode keyBackward;
+    private KeyCode keyTurnLeft;
+    private KeyCode keyTurnRight;
+    
+    
+    private Rigidbody2D rb;
+
+    public BoatController()
+    {
+        keyForward = KeyCode.W;
+        keyTurnRight = KeyCode.D;
+        keyBackward = KeyCode.S;
+        keyTurnLeft = KeyCode.A;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        handleMove();
+        handleRotation();
+    }
+
+    private void handleRotation()
+    {
+        var torque = 0f;
+        
+        if(Input.GetKey(keyTurnLeft))
+        {
+            torque += 1f;
+        }
+        
+        if(Input.GetKey(keyTurnRight))
+        {
+            torque -= 1f;
+        }
+        
+        rb.AddTorque(torque * Time.deltaTime, ForceMode2D.Force);
+    }
+    
+    private void handleMove()
+    {
+        var move = Vector3.zero;
+        
+        if (Input.GetKey(keyBackward))
+        {
+            move += transform.up * acceleration;
+        }
+        
+        if(Input.GetKey(keyForward))
+        {
+            move -= transform.up * (acceleration / 2);
+        }
+        
+        rb.AddForce(move, ForceMode2D.Force);
+    }
+}
