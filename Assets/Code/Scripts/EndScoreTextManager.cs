cb316af479331d41845e6ec8f3da046688f897ba
zaayko <zaayko17@gmail.com>
2025-03-16 10:50:07 +0100

    Graphic adjustments

--- Assets/Code/Scripts/EndScoreTextManager.cs
+++ Assets/Code/Scripts/EndScoreTextManager.cs
@@ -23,7 +23,7 @@ public class EndScoreTextManager : MonoBehaviour
     {
         // Set end score text
         int endsScore = GameManager.Instance.getScore();
-        instance.endScoreText.text = "You failed to protect the Titanic! Your score is " + endsScore;
+        instance.endScoreText.text = "You failed to protect<br>the Titanic! Your score is : <br><size=40><br><size=110><color=white>" + endsScore;
         
         // Display end score text
         instance.endScoreButton.gameObject.SetActive(true);
